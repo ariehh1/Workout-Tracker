48e41a0fff1d4333a2f8535f963d1499b6ae7398
Ari Horowitz <ariehh@comcast.net>
2020-03-25 18:55:38 -0700

    reformatted code in models index.js and starting to write workout.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const Workout = require(`./workout`);
+
 module.exports = {
-  Workout
+  Workout: require("./workout")
 };
--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,22 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    type: {
+
+    },
+
+
+}
+});
+
+const Workout = mongoose.model(`Workout`, workoutSchema);
+
+module.exports = Workout;
+
 // user adds their exercise
 
 // name of the exercise
