1e32235b27f690e21eeb968205591afb4a77e07e
Ari Horowitz <ariehh@comcast.net>
2020-03-27 19:09:58 -0700

    html.js routes completed

--- routes/html.js
+++ routes/html.js
@@ -1,3 +1,14 @@
-// anything with site loading
+"use strict";
 
-// exerciese html and stats html
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/populatedb`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
