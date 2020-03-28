8c0e92611ad7e980af51bc8e7f671aee76272385
Ari Horowitz <ariehh@comcast.net>
2020-03-27 17:03:24 -0700

    include virtual properties workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,63 +1,59 @@
 "use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
 
     exercises: [
-        {
-            type: {
-            type: String,
-            trim: true,
-            required: `Enter a Resistance or Cardio exercise`
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: `Enter a name for the exercise`
-                },
-                duration: {
-                    type: Number,
-                    required: `Enter a number for exercise duration`
-                },
-                weight: {
-                    type: Number
-                },
-                reps: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                }
-        }]
-},
-{
-
-    
-}
-
-
-        
-
-}
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: `Enter a Resistance or Cardio exercise`
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: `Enter a name for the exercise`
+        },
+        duration: {
+          type: Number,
+          required: `Enter a number for exercise duration`
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-const Workout = mongoose.model(`Workout`, workoutSchema);
+const workout = mongoose.model("workout", workoutSchema);
 
-module.exports = Workout;
-
-// user adds their exercise
-
-// name of the exercise
-// time
-// weight
-// sets
+module.exports = workout;
