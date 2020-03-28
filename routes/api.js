8fd5a82e15f56fc57eb74763b8e0d5ea03f91508
Ari Horowitz <ariehh@comcast.net>
2020-03-27 18:56:12 -0700

    update and second get route api.js

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@ router.get("/api/workouts", (req, res) => {
     .then(results => res.json(results))
     .catch(err => res.json(err));
 });
+
 // Post
 router.post("/api/workouts", (req, res) => {
   workout
@@ -19,3 +20,19 @@ router.post("/api/workouts", (req, res) => {
 });
 
 // Update
+router.put("/api/workouts/:id", (req, res) => {
+  workout
+    .findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+    .then(results => res.json(results))
+    .catch(err => res.json(err));
+});
+
+// Get range
+router.get("/api/workouts/range", (req, res) => {
+  workout
+    .find()
+    .then(results => res.json(results))
+    .catch(err => res.json(err));
+});
+
+module.exports = router;
