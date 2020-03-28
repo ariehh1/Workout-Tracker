7193a3ca236386f651fbd29a576b3f02ac146f4f
Ari Horowitz <ariehh@comcast.net>
2020-03-27 18:47:39 -0700

    first get and post routes api.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,21 @@
-// Get
+"use strict";
 
+const router = require("express").Router();
+const workout = require("../models/workout.js");
+
+// Get
+router.get("/api/workouts", (req, res) => {
+  workout
+    .find()
+    .then(results => res.json(results))
+    .catch(err => res.json(err));
+});
 // Post
+router.post("/api/workouts", (req, res) => {
+  workout
+    .create({})
+    .then(results => res.json(results))
+    .catch(err => res.json(err));
+});
 
 // Update
